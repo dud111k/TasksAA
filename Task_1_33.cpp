0c375494acf1bd2dfdeaec6f673820cff1473bf7
dud111k <hermorzhoviy36@gmail.com>
2024-12-09 23:08:36 +0300

    Add files via upload

---
+++ Task_1_33.cpp
@@ -0,0 +1,20 @@
+﻿// Task_1_33.cpp: определяет точку входа для приложения.
+//
+
+#include "Task_1_33.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int population;
+	double area, density;
+	cout << "Введите количество жителей государства: ";
+	cin >> population;
+	cout << "Введите площадь государства в кв. км: ";
+	cin >> area;
+	density = population / area;
+	cout << "Плотность государства равна " << density << " чел. на кв. км." << endl;
+	return 0;
+}
