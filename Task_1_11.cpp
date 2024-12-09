c9ade2de7b554f01fde0bf894b50d45fc8f02fff
dud111k <hermorzhoviy36@gmail.com>
2024-12-09 22:26:17 +0300

    Add files via upload

---
+++ Task_1_11.cpp
@@ -0,0 +1,16 @@
+﻿// Task_1_11.cpp: определяет точку входа для приложения.
+//
+
+#include "Task_1_11.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "52" << endl;
+	cout << "42" << endl;
+	cout << "65" << endl;
+	cout << "13" << endl;
+
+	return 0;
+}
