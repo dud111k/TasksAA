e60a39f1ad50f1f30d26a1f992c66ccff23b8e50
dud111k <hermorzhoviy36@gmail.com>
2024-12-11 02:06:17 +0300

    Add files via upload

---
+++ Task_2_5.cpp
@@ -0,0 +1,18 @@
+﻿// Task_2_5.cpp: определяет точку входа для приложения.
+//
+
+#include "Task_2_5.h"
+#include "iostream"
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+	int days = 234;
+	int weeks;
+	setlocale(0, "");
+	weeks = floor(days / 7);
+	cout << weeks << endl;
+	return 0;
+}
