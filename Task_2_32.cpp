9812a837e1480618368d015e9963368c0c79bd08
dud111k <hermorzhoviy36@gmail.com>
2024-12-14 00:53:42 +0300

    Update Task_2_32.cpp

--- Task_2_32.cpp
+++ Task_2_32.cpp
@@ -1,12 +1,13 @@
 ﻿// Task_2_32.cpp: определяет точку входа для приложения.
-//
+//В трехзначном числе x зачеркнули его последнюю цифру. Когда в оставшемся двузначном числе переставили цифры, а затем приписали к ним слева послед-
+//нюю цифру числа x, то получилось число 654. Найти число x.
 
 #include "Task_2_32.h"
 
 using namespace std;
 
 int main()
-{
+{	
 	int number = 654;
 	int n1;
 	int n2;
@@ -16,6 +17,6 @@ int main()
 	n2 = number % 10;
 	n3 = number - n1 * 100 - n2;
 	number_start = n2 * 100 + n3 + n1;
-	cout << number_start << endl;
+	cout << "x = " << number_start << endl;
 	return 0;
 }
