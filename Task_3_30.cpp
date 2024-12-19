ef67bda3dbaac8cb76c7d48d8dd9355bc3461190
dud111k <hermorzhoviy36@gmail.com>
2024-12-19 15:35:14 +0300

    Task_3_30

---
+++ Task_3_30.cpp
@@ -0,0 +1,33 @@
+﻿
+/* Записать условие, которое является истинным, когда:
+а) целое А кратно двум или трем;
+б) целое А не кратно трем и оканчивается нулем.
+*/
+
+#include "Task_3_30.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool testOne(int x)
+{
+	if (x % 2 == 0 || x % 3 == 0) { return true;}
+	else { return false; }
+}
+
+bool testTwo(int x) {
+	if (x % 10 == 0 && x % 3 != 0) { return true;}
+	else { return false;}
+}
+
+int main()
+{
+	int number;
+	cout << "Enter number: ";
+	cin >> number;
+	cout << "a) " << testOne(number) << endl;
+	cout << "b) " << testTwo(number) << endl;
+
+	return 0;
+}
