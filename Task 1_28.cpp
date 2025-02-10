e74dd34525a5206faa3dedf716f35a49a594140b
dud111k <hermorzhoviy36@gmail.com>
2025-02-10 22:56:44 +0300

    Update Task 1_28.cpp

--- Task 1_28.cpp
+++ Task 1_28.cpp
@@ -4,15 +4,16 @@
 #include "math.h"
 using namespace std;
 
-int main(int argc, double argv[])
+int main(int argc, char* argv[])
 {
+	double side_l = atof(argv[1]);
 	double cube_v;
 	double cube_s;
 	setlocale(0, "");
 	cout << "Длина ребра куба: " << argv[1] << endl;
-	cube_v = pow(argv[1], 3);
-	cube_s = 4 * pow(argv[1], 2);
-	cout << "Площадь куба равна " << cube_s << " кв. см." << endl;
-	cout << "Объём куба равен " << cube_v << " кв. см." << endl;
+	cube_v = pow(side_l, 3);
+	cube_s = 4 * pow(side_l, 2);
+	cout << "Площадь куба равна " << cube_s << " куб. см." << endl;
+	cout << "Объём куба равен " << cube_v << " куб. см." << endl;
 	return 0;
 }
