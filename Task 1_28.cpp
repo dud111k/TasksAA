fee86cb65b18c271566a23ce1beee545b3f559b2
dud111k <hermorzhoviy36@gmail.com>
2025-02-17 22:33:16 +0300

    Update Task 1_28.cpp

--- Task 1_28.cpp
+++ Task 1_28.cpp
@@ -6,14 +6,19 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	double side_l = atof(argv[1]);
-	double cube_v;
-	double cube_s;
-	setlocale(0, "");
-	cout << "Длина ребра куба: " << argv[1] << endl;
-	cube_v = pow(side_l, 3);
-	cube_s = 4 * pow(side_l, 2);
-	cout << "Площадь куба равна " << cube_s << " кв. см." << endl;
-	cout << "Объём куба равен " << cube_v << " куб. см." << endl;
+	if (argc >= 2) {
+		double side_l = atof(argv[1]);
+		double cube_v;
+		double cube_s;
+		setlocale(0, "");
+		cout << "Длина ребра куба: " << argv[1] << endl;
+		cube_v = pow(side_l, 3);
+		cube_s = 4 * pow(side_l, 2);
+		cout << "Площадь куба равна " << cube_s << " куб. см." << endl;
+		cout << "Объём куба равен " << cube_v << " куб. см." << endl;
+	}
+	else {
+		cout << "Не задана длина ребра куба" << endl;
+	}
 	return 0;
 }
